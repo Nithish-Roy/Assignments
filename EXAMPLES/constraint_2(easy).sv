7954037d96667dffc4fb3d006c00f6e63d338c64
Nithish Roy <roynithish70@gmail.com>
2025-04-03 09:38:50 +0530

    Update constraint_2(easy).sv

--- EXAMPLES/constraint_2(easy).sv
+++ EXAMPLES/constraint_2(easy).sv
@@ -9,7 +9,12 @@ class test;
 
   rand bit [6:0]  c;
 
+  //   constraint for_all {
+  //   unique {a, b, c};
+  // }
+
   constraint for_all {
+    
   						a != b;
   						
   						b != c;
@@ -45,18 +50,4 @@ module tester;
 // # KERNEL: a =  70 b =  61 c =  27
 // # KERNEL: a =  10 b =  32 c =  76
 // # KERNEL: a =  14 b =  11 c =   4
-// # KERNEL: a =  91 b =  84 c =   7
-// # KERNEL: a =  50 b =   7 c =   9
-// # KERNEL: a =   1 b =  24 c =  87
-// # KERNEL: a =  14 b =  48 c =  62
-// # KERNEL: a = 125 b =  81 c =  51
-// # KERNEL: a =  19 b =  20 c =  99
-// # KERNEL: a =  36 b =  51 c =  24
-// # KERNEL: a = 104 b = 106 c =  46
-// # KERNEL: a = 102 b =  37 c = 112
-// # KERNEL: a =   6 b =  18 c =  93
-// # KERNEL: a =  91 b =   6 c =   4
-// # KERNEL: a = 105 b =  40 c =  71
-// # KERNEL: a = 102 b =  96 c =  79
-// # KERNEL: a = 122 b =  61 c =  12
-// # KERNEL: a =  33 b =  25 c =  38
+
