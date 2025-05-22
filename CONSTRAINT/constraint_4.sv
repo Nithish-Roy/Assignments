a60304f9ed2dbf6e5fc3c4e4c6bdbe3160e96711
Nithish Roy <roynithish70@gmail.com>
2025-05-22 20:02:56 +0530

    Create constraint_4.sv

---
+++ CONSTRAINT/constraint_4.sv
@@ -0,0 +1,52 @@
+//fibonacci series
+
+class test;
+  
+  rand int num[20];
+  
+  constraint for_number {foreach(num[i])
+
+    if(i < 2)
+       
+      num[i] == 0;
+	
+                         else  if(i== 2)
+                           
+                           num[i] == 1;
+      
+                         else 
+                           
+                           num[i] == num[i-1] + num[i-2]+ num[i-3] ;
+                        }
+    
+    
+  function void display();
+   
+    foreach(num[i])
+      
+      $display("number = %0d", num[i]);
+    
+  endfunction
+  
+endclass
+
+module tester;
+  
+  initial begin 
+    
+    test tst;
+    
+    tst = new();
+    
+    repeat(10) begin 
+      
+      tst.randomize();
+      
+      tst.display();
+      
+    end
+    
+  end
+  
+endmodule
+
